0c705b72b2c294c47fa927e30014e49c15ea8950
TKO-Cuber <28767546+TKO-Cuber@users.noreply.github.com>
2017-10-18 12:00:43 -0400

    Added windows version

---
+++ jelbrek.bat
@@ -0,0 +1,152 @@
+cls
+
+:A
+
+color 0a
+
+cls
+
+@echo off
+
+echo Wscript.Sleep
+
+echo Wscript.Sleep 5000>C:sleep5000.vbs
+
+echo Wscript.Sleep 3000>C:sleep3000.vbs
+
+echo Wscript.Sleep 4000>C:sleep4000.vbs
+
+echo Wscript.Sleep 2000>C:sleep2000.vbs
+
+echo Wscript.Sleep 1000>C:sleep1000.vbs
+
+echo Wscript.Sleep 500>C:sleep500.vbs
+
+echo Wscript.Sleep 100>C:sleep100.vbs
+
+cd %systemroot%System32
+
+dir
+
+cls
+
+start /w wscript.exe C:sleep3000.vbs
+
+echo Welcome to the Etas0n jelbrek!
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo wen eta?
+
+start /w wscript.exe C:sleep3000.vbs
+
+echo eta son
+
+start /w wscript.exe C:sleep3000.vbs
+
+pause
+
+echo please plug in any iPhone to continue with the jelbrek.
+
+pause
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo no device detected. Please unplug and replug device.
+
+pause
+
+echo device detected- jelbrek started...
+
+start /w wscript.exe C:sleep2000.vbs
+
+echo:
+
+echo: 
+
+echo escaping sandbox
+
+start /w wscript.exe C:sleep2000.vbs
+
+echo passing bad port...
+
+start /w wscript.exe C:sleep500.vbs
+
+echo running kernel exploit
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo bypass KPP
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo patching sandbox
+
+start /w wscript.exe C:sleep500.vbs
+
+echo patching AMFI
+
+start /w wscript.exe C:sleep500.vbs
+
+echo patching tfp0
+
+start /w wscript.exe C:sleep100.vbs
+
+echo installing substrate
+
+start /w wscript.exe C:sleep100.vbs
+
+echo adding substrate permission files
+
+start /w wscript.exe C:sleep3000.vbs
+
+echo adding root
+
+start /w wscript.exe C:sleep100.vbs
+
+echo adding terminal shell
+
+start /w wscript.exe C:sleep100.vbs
+
+echo installing ssh
+
+start /w wscript.exe C:sleep100.vbs
+
+echo allowing ssh to run at boot
+
+start /w wscript.exe C:sleep100.vbs
+
+echo starting ssh daemon
+
+start /w wscript.exe C:sleep100.vbs
+
+echo adding repositories
+
+start /w wscript.exe C:sleep100.vbs
+
+echo running additional patches...
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo installing Cydia...
+
+start /w wscript.exe C:sleep5000.vbs
+
+echo done. Enjoy your jelbrek.
+
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
